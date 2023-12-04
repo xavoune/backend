91756e47892c2dd926847b8e8f0c9b3ff5c22d42
Xavoune <lespade.xavier@gmail.com>
2023-12-04 15:12:10 +0100

    correction bug connection

--- app.js
+++ app.js
@@ -1,8 +1,27 @@
 const express = require('express');
+const mongoose = require('mongoose');
+
+const userRoutes = require('./routes/user');
+const saucesRoutes = require('./routes/sauce');
+
+mongoose.connect('mongodb+srv://testOC:1234567890@cluster0.t8ie8bs.mongodb.net/?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+.then(() => console.log('Connexion à MongoDB réussie !'))
+.catch(() => console.log('Connexion à MongoDB échouée !'));
 
 const app = express();
 
 app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
+});
+
+app.use(express.json())
+
+/*app.use((req, res, next) => {
   console.log('Requête reçue !');
   next();
 });
@@ -19,6 +38,10 @@ app.use((req, res, next) => {
 
 app.use((req, res, next) => {
   console.log('Réponse envoyée avec succès !');
-});
+});*/
+
+
+app.use('/api/auth', userRoutes);
+app.use('/api/sauces', saucesRoutes);
 
 module.exports = app;
---
+++ routes/sauce.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+
+//const auth = require('../middleware/auth');
+
+// router.get('/', sauceCtrl.getAllSauces);
+// router.get('/:id', saucesCtrl.getSaucesById);
+// router.post('/', sauceCtrl.createSauce);
+// router.put('/', sauceCtrl.modifySauce)
+// router.delete('/:id', sauceCtrl.deleteSauce)
+// router.post(':id/like', sauceCtrl.likeASauce)
+
+module.exports = router;
