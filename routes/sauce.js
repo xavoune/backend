22d16371b2f6e703df74088561370ef55f07a03b
Xavoune <lespade.xavier@gmail.com>
2023-12-12 13:43:20 +0100

    activation routes

--- routes/sauce.js
+++ routes/sauce.js
@@ -7,10 +7,10 @@ const multer = require('../middlewares/multer-config');
 const sauceCtrl = require('../controllers/sauce');
 
 router.get('/', auth, sauceCtrl.getAllSauces);
-// router.get('/:id', auth, saucesCtrl.getSaucesById);
-// router.post('/', auth, multer, sauceCtrl.createSauce);
-// router.put('/', auth, sauceCtrl.modifySauce)
-// router.delete('/:id', auth, sauceCtrl.deleteSauce)
+router.get('/:id', auth, sauceCtrl.getSaucesById);
+router.post('/', auth, multer, sauceCtrl.createSauce);
+router.put('/:id', auth, sauceCtrl.modifySauce)
+router.delete('/:id', auth, sauceCtrl.deleteSauce)
 // router.post(':id/like', auth, sauceCtrl.likeASauce)
 
 module.exports = router;
